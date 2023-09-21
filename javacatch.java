c483afb7eb5b4617f43570b337b7025db5e019b0
백동흔 <128360692+beakdongH@users.noreply.github.com>
2023-09-21 19:11:07 +0900

    잡기

--- javacatch.java
+++ javacatch.java
@@ -179,3 +179,76 @@ public class EffectOfReferenceArgument {
     }
 }
 
+
+package ch06;
+
+public class MethodOverloading {
+    public static void main(String[] args){
+        print();
+    }
+
+    public static void print(){
+        System.out.println("데이터가 없습니다");
+    }
+
+    public static void print(int a){
+        System.out.println("1234");
+    }
+
+    public static void print(double a){
+        System.out.println("5678");
+    }
+//    바로 위의 것과 메서드 시그니쳐가 똑같기 때문에 정의 불가능.
+//    public static void print(double d){
+//        System.out.println("d5678");
+//}
+    public static void print(int a, int b){
+        System.out.println("1234");
+    }
+    //메서드 시그니쳐가 다르다는 것은 이름은 똑같지만 매개변수의종류나 숫자가 다름을 의미
+
+//    public static int print(int a, int b){
+//        System.out.println("1234");
+//        return a;
+//    }
+    //반환형이 다르다는 것은 호출할 떄 헷갈리기 때문에 시그니쳐가 같다라고 할 수 있다.
+}
+
+
+package ch07;
+
+import java.util.SplittableRandom;
+
+public class FlexibleSizeArrayArgument {
+    public static void main(String[] args){
+        //메서드 오버로딩을 이용해서 동일한 이름의 메서드를 다양하게 호출
+        method1(1,2);
+        method1(1,2,3);
+        method1();
+
+        method2("안녕","방가");
+        method2("땡큐","베리","감사");
+        method2();
+
+
+    }
+    //와 만약에 한개짜리부터 백개짜리까지 다있다면..
+    //총 100가지의 시그니처를 만들어야하는거야?? 겁나 불편하네?
+    //어떤 개발자가 새로운 문법을 만들었습니다.
+    //이름하야 가변길이 배열 매개변수
+    public static void method1(int...array){
+        for(int k: array){
+            System.out.print(k+" ");
+        }
+        System.out.println();
+    }
+
+    public static void method2(String...array){
+        for(String k: array){
+            System.out.print(k+" ");
+        }
+        System.out.println();
+    }
+
+}
+
